adf090ca136b8451bc0cab10303a12756d826f6a
Bruce Zhang <zttt183525594@gmail.com>
2025-01-02 20:41:05 +0800

    merge

--- generator/script.py
+++ generator/script.py
@@ -4,6 +4,7 @@ import sys
 
 import requests
 from typing import List
+from pathlib import Path
 
 import yaml
 
@@ -12,7 +13,6 @@ def parse_proxies_from_sub(sub: str) -> List[dict]:
     response = requests.get(sub, headers={'User-Agent': 'clash.meta'})
     sub_text = response.text
     data = yaml.load(sub_text, Loader=yaml.FullLoader)
-    print(data)
     proxies = data.get('proxies')
     if proxies is None:
         return []
@@ -30,3 +30,11 @@ def parse_proxies_from_env() -> List[dict]:
     for sub in sub_list:
         proxy_list.extend(parse_proxies_from_sub(sub))
     return proxy_list
+
+
+def merge_proxies_into_template(proxies: List[dict]) -> str:
+    with (Path(__file__).parent / 'template.yaml').open('r') as f:
+        template = f.read()
+        data = yaml.load(template, Loader=yaml.FullLoader)
+        data['proxies'] = proxies
+        return yaml.dump(data)
--- tests/test_script.py
+++ tests/test_script.py
@@ -15,9 +15,16 @@ os.environ['SUBSCRIPTIONS'] = json.dumps([TEST_SUBSCRIPTION])
 #     assert sub_list[0].get('server') is not None
 
 
-def test_parse_proxies_from_env():
+# def test_parse_proxies_from_env():
+#     sub_list = script.parse_proxies_from_env()
+#     assert type(sub_list) == list
+#     assert len(sub_list) > 0
+#     assert type(sub_list[0]) == dict
+#     assert sub_list[0].get('server') is not None
+
+
+def test_merge_proxies_into_template():
     sub_list = script.parse_proxies_from_env()
-    assert type(sub_list) == list
-    assert len(sub_list) > 0
-    assert type(sub_list[0]) == dict
-    assert sub_list[0].get('server') is not None
+    template = script.merge_proxies_into_template(sub_list)
+    print(template)
+    assert type(template) == str
