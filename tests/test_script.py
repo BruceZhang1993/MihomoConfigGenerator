3557a09de08be040983ca8011835252a37433b0c
Bruce Zhang <zttt183525594@gmail.com>
2025-01-06 21:48:41 +0800

    unit test mihomo delay test

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,11 +1,16 @@
 import json
 import os
+from pathlib import Path
+from time import sleep
+
+import yaml
 
 from generator import script
+from generator.mihomo import MihomoCore
 
 TEST_SUBSCRIPTION = 'https://raw.githubusercontent.com/BruceZhang1993/MihomoConfigGenerator/refs/heads/master/example/subscribe.yml'
 os.environ['SUBSCRIPTIONS'] = json.dumps([TEST_SUBSCRIPTION])
-f = open('example/template.yml', 'r')
+f = open(Path(__file__).parent.parent / "example" / "template.yml", 'r')
 os.environ['TEMPLATE'] = f.read()
 f.close()
 
@@ -30,3 +35,21 @@ def test_merge_proxies_into_template():
     sub_list = script.parse_proxies_from_env()
     template = script.merge_proxies_into_template(sub_list)
     assert type(template) == str
+
+
+def test_proxy_delay_test():
+    core = MihomoCore()
+    core.start_mihomo_core_process()
+    assert core.is_running
+    with open(Path(__file__).parent.parent / "example" / "generated.yml", 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+        core.put_configs(data)
+    sleep(3)
+    print("==============================")
+    for k, v in core.proxies()['proxies'].items():
+        if k in ('COMPATIBLE', 'DIRECT', 'GLOBAL', 'PASS', 'PROXY', 'REJECT', 'REJECT-DROP'):
+            continue
+        result = core.proxy_delay(k)
+        print(k, '=>', result)
+    sleep(3)
+    core.stop()
